15436fb8da8be582528511be1fbf91cb17337d39
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 16:11:04 -0300

    a

--- code/mapa.py
+++ code/mapa.py
@@ -21,7 +21,7 @@ div.stButton > button:first-child {
     height: 3em;
     width: 12em;
     border-radius:10px;
-    border:3px solid #000000;
+    border:3px solid #ce1126;
     font-size:20px;
     font-weight: bold;
     margin: auto;
@@ -48,5 +48,32 @@ st.markdown('<p></p>', unsafe_allow_html = True)
 st.markdown('<p></p>', unsafe_allow_html = True)
 
 c = st.button("Button 2")
+
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #000000;
+    color: white;
+    height: 3em;
+    width: 12em;
+    border-radius:10px;
+    border:3px solid #ce1126;
+    font-size:20px;
+    font-weight: bold;
+    margin: auto;
+    display: block;
+}
+
+div.stButton > button:hover {
+	background:linear-gradient(to bottom, #ce1126 5%, #ff5a5a 100%);
+	background-color:#ce1126;
+}
+
+div.stButton > button:active {
+	position:relative;
+	top:3px;
+}
+
+</style>""", unsafe_allow_html=True)
 
 d = st.button("teste")
