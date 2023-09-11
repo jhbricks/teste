b345e0ecc700384554bef6fb86c6890eeb439b2d
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 16:39:01 -0300

    a

--- code/mapa.py
+++ code/mapa.py
@@ -10,7 +10,7 @@ def ChangeButtonColour(widget_label, font_color, background_color='transparent')
                     elements[i].style.color ='{font_color}';
                     elements[i].style.background = '{background_color}';
                     elements[i].height = 3em;
-                    elements[i].widtg = 12em
+                    elements[i].width = 12em
                 
                 }}
             }}
