1da999e7a223ed032258052db15be867930e1f92
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 18:29:53 -0300

    a

--- code/buttons 1.py
+++ code/buttons 1.py
@@ -121,4 +121,41 @@ ChangeButtonStyle('second button', '#ce1126', global_button_style)
 ChangeButtonStyle('fourth button', '#354b75', global_button_style)
 
 
+#############################################################33
+import streamlit as st
 
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #faecca;
+    color: black;
+    height: 3em;
+    width: 12em;
+    border-radius:10px;
+    border:3px solid #faecca;
+    font-size:20px;
+    font-weight: bold;
+    margin: auto;
+    display: block;
+}
+
+div.stButton > button:hover {
+	background:linear-gradient(to bottom, #faecca 5%, #f0ede6 100%);
+	background-color:##faecca;
+}
+
+div.stButton > button:active {
+	position:relative;
+	top:3px;
+}
+
+</style>""", unsafe_allow_html=True)
+
+st.button("Button 1")
+
+
+st.markdown('<p></p>', unsafe_allow_html = True)
+st.markdown('<p></p>', unsafe_allow_html = True)
+st.markdown('<p></p>', unsafe_allow_html = True)
+
+st.button("Button 2")
--- code/mapa.py
+++ code/mapa.py
@@ -1,37 +1,17 @@
 import streamlit as st
-
-m = st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: #faecca;
-    color: black;
-    height: 3em;
-    width: 12em;
-    border-radius:10px;
-    border:3px solid #faecca;
-    font-size:20px;
-    font-weight: bold;
-    margin: auto;
-    display: block;
-}
-
-div.stButton > button:hover {
-	background:linear-gradient(to bottom, #faecca 5%, #f0ede6 100%);
-	background-color:##faecca;
-}
-
-div.stButton > button:active {
-	position:relative;
-	top:3px;
-}
-
-</style>""", unsafe_allow_html=True)
-
-st.button("Button 1")
+from streamlit_extras.switch_page_button import switch_page
 
 
-st.markdown('<p></p>', unsafe_allow_html = True)
-st.markdown('<p></p>', unsafe_allow_html = True)
-st.markdown('<p></p>', unsafe_allow_html = True)
+st.set_page_config(layout="wide",page_title="Mapa da Desigualdade")
 
-st.button("Button 2")
+#Remove os espaços em branco no topo
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
