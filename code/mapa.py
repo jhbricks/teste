8759636d6579efa9659ac31d9211c331ccb32adb
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 17:17:38 -0300

    a

--- code/mapa.py
+++ code/mapa.py
@@ -2,22 +2,13 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def ChangeButtonStyle(widget_label, background_color, style):
-    hover_color = f"rgba({int(background_color[1:3], 16)}, {int(background_color[3:5], 16)}, {int(background_color[5:], 16)}, 0.2)"
     htmlstr = f"""
-        <style>
-            .button-{widget_label} {{
-                background: {background_color};
-                {style}
-            }}
-            .button-{widget_label}:hover {{
-                background: {hover_color};
-            }}
-        </style>
         <script>
             var elements = window.parent.document.querySelectorAll('button');
             for (var i = 0; i < elements.length; ++i) {{ 
                 if (elements[i].innerText == '{widget_label}') {{ 
-                    elements[i].classList.add('button-{widget_label}');
+                    elements[i].style.background = '{background_color}';
+                    {style}
                 }}
             }}
         </script>
@@ -26,15 +17,15 @@ def ChangeButtonStyle(widget_label, background_color, style):
 
 # Define the style you want to apply to all buttons
 global_button_style = """
-    color: white;
-    height: 3em;
-    width: 12em;
-    border-radius: 10px;
-    border: 3px solid #ce1126;
-    font-size: 20px;
-    font-weight: bold;
-    margin: auto;
-    display: block;
+    elements[i].style.color = 'white';
+    elements[i].style.height = '3em';
+    elements[i].style.width = '12em';
+    elements[i].style.borderRadius = '10px';
+    elements[i].style.border = '3px solid #ce1126';
+    elements[i].style.fontSize = '20px';
+    elements[i].style.fontWeight = 'bold';
+    elements[i].style.margin = 'auto';
+    elements[i].style.display = 'block';
 """
 
 # Create the buttons
