ab6a64a5e41b69c3038f3c775c9956b4f5b47b1f
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 18:02:52 -0300

    a

--- code/mapa.py
+++ code/mapa.py
@@ -1,33 +1,34 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-def ChangeButtonStyle(widget_label, background_color, style):
+def ChangeButtonStyle(widget_label, background_color):
     htmlstr = f"""
+        <style>
+            .custom-button {{
+                background-color: {background_color};
+                color: white;
+                height: 3em;
+                width: 12em;
+                border-radius: 10px;
+                font-size: 20px;
+                font-weight: bold;
+                margin: auto;
+                display: block;
+            }}
+            .custom-button:hover {{
+                background-color: ({background_color}, 0.2);
+            }}
+        </style>
         <script>
             var elements = window.parent.document.querySelectorAll('button');
             for (var i = 0; i < elements.length; ++i) {{ 
                 if (elements[i].innerText == '{widget_label}') {{ 
-                    elements[i].style.background = '{background_color}';
-                    {style}
+                    elements[i].classList.add('custom-button');
                 }}
             }}
         </script>
         """
     components.html(f"{htmlstr}", height=0, width=0)
-
-# Define the style you want to apply to all buttons
-global_button_style = """
-    elements[i].style.color = 'white';
-    elements[i].style.height = '3em';
-    elements[i].style.width = '12em';
-    elements[i].style.borderRadius = '10px';
-    elements[i].style.fontSize = '20px';
-    elements[i].style.fontWeight = 'bold';
-    elements[i].style.margin = 'auto';
-    elements[i].style.display = 'block';
-    .button-hover:
-        elements[i].style.background: (('background_color' 5%))
-"""
 
 # Create the buttons
 cols = st.columns(2)
@@ -35,6 +36,6 @@ cols = st.columns(2)
 cols[0].button('second button', key='b2')
 cols[1].button('fourth button', key='b4')
 
-# Apply the global style to all buttons (This will apply the style to all buttons initially)
-ChangeButtonStyle('second button', '#ce1126', global_button_style)
-ChangeButtonStyle('fourth button', '#354b75', global_button_style)
+# Apply the custom style to all buttons (This will apply the style to all buttons initially)
+ChangeButtonStyle('second button', st.get_option("theme.secondaryBackgroundColor"))
+ChangeButtonStyle('fourth button', st.get_option("theme.primaryBackgroundColor"))
