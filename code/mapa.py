4955e7da3c5960512cc1e0ceb4bac75b56b659cf
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 10:52:53 -0300

    Update mapa.py

--- code/mapa.py
+++ code/mapa.py
@@ -25,85 +25,81 @@ k=100
 cmap='YlOrBr'
 fields=['Município','População']
 title= 'População residente'
-
-@st.cache_data
-
-def mapagvf(area, arq, ind, scheme, k, cmap, fields, title):
 ######encaminha o geojson da area
-    if area == 'PR':
-        arq_g = "https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/PR.geojson"
-    else:
-        area = 'NTC'
-        arq_g = "https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/NTC.geojson"
+#    if area == 'PR':
+#        arq_g = "https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/PR.geojson"
+ #   else:
+  #      area = 'NTC'
+   #     arq_g = "https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/NTC.geojson"
 
 #######MERGE geojson e csv
-    arq_csv = pd.read_csv(arq)
-    arq_geojson = gpd.read_file(arq_g)
-    data = arq_geojson.merge(arq_csv, on="Município")
+    
+arq_csv = pd.read_csv(arq)
+arq_geojson = gpd.read_file(arq_g)
+data = arq_geojson.merge(arq_csv, on="Município")
 
 #######LAT E LON CENTRAIS
-    ponto_central = arq_geojson.geometry.centroid
-    lat = ponto_central.iloc[0].y
-    lon = ponto_central.iloc[0].x
+ponto_central = arq_geojson.geometry.centroid
+lat = ponto_central.iloc[0].y
+lon = ponto_central.iloc[0].x
     
-    if not isinstance(data,gpd.GeoDataFrame):
-        print("O arquivo não é um GeoDataFrame")
-        exit()
+if not isinstance(data,gpd.GeoDataFrame):
+   print("O arquivo não é um GeoDataFrame")
+   exit()
 
 #Lat e Lon Centrais
-    ponto_central = data.geometry.centroid
-    lat = ponto_central.iloc[0].y
-    lon = ponto_central.iloc[0].x
+ponto_central = data.geometry.centroid
+lat = ponto_central.iloc[0].y
+lon = ponto_central.iloc[0].x
 
-    m = leafmap.Map(width=900, height=600, center=[lat, lon],
-                    draw_control=False,
-                    measure_control=False,
-                    fullscreen_control=False,
-                    attribution_control=True)
-
+ m = leafmap.Map(width=900, height=600, center=[lat, lon],
+                 draw_control=False,
+                 measure_control=False,
+                 fullscreen_control=False,
+                 attribution_control=True)
         # ZOOM 
-    if area == PR:
-        m.zoom_to_bounds((-47.98, -22.44, -54.67, -26.80))
-    else:
-        m.zoom_to_bounds((-47.87,-24.96,-48.54, -25.85))
+#if area == PR:
+m.zoom_to_bounds((-47.98, -22.44, -54.67, -26.80))
+# else:
+#        m.zoom_to_bounds((-47.87,-24.96,-48.54, -25.85))
 
-    style_style = {"stroke": True,           #linha da borda vai ser desenhada
-                   "color": "#000000",       #cor da linha (preto)
-                   "weight": 2,              #espessura 2 px
-                  #"opacity": 0,             #opacidade da borda (1 = totalmente opaca)
-                   "fillOpacity": 1,       #opacidade do preenchimento (0.1 = 10% opaca)
-                   "clickable": True,
-                   }
+style_style = {"stroke": True,           #linha da borda vai ser desenhada
+               "color": "#000000",       #cor da linha (preto)
+               "weight": 2,              #espessura 2 px
+               #"opacity": 0,             #opacidade da borda (1 = totalmente opaca)
+               "fillOpacity": 1,       #opacidade do preenchimento (0.1 = 10% opaca)
+               "clickable": True,
+               }
 
     
-    m.add_data(data=data,
-               column=ind,
-               scheme=scheme,
-               k=k,
-               cmap=cmap,
-               fields=fields,
-               legend_title=title,
-               legend_position='Bottomright',
-               layer_name=title,
-               style = style_style,
-               )
+ m.add_data(data=data,
+            column=ind,
+            scheme=scheme,
+            k=k,
+            cmap=cmap,
+            fields=fields,
+            legend_title=title,
+            legend_position='Bottomright',
+            layer_name=title,
+            style = style_style,
+            )
 
         ########VALORES DE MX E MN DAS VARIAVEIS
-    max_value = data[ind].max()
-    min_value = data[ind].min()
-    max_municipio = data.loc[data[ind] == max_value, "Município"].iloc[0]
-    min_municipio = data.loc[data[ind] == min_value, "Município"].iloc[0]
+max_value = data[ind].max()
+min_value = data[ind].min()
+max_municipio = data.loc[data[ind] == max_value, "Município"].iloc[0]
+min_municipio = data.loc[data[ind] == min_value, "Município"].iloc[0]
 
         #####ADICIONAR MX E MN NO MAPA
-    folium.Marker([data.loc[data[ind] == max_value, "Y"].iloc[0],
-                   data.loc[data[ind] == max_value, "X"].iloc[0]],
-                   popup=f"Maior valor: {max_value}<br>{max_municipio}",
-                   icon=folium.Icon(color="darkpurple", icon="arrow-up"),
-                   ).add_to(m)
-    folium.Marker([data.loc[data[ind] == min_value, "Y"].iloc[0],
-                   data.loc[data[ind] == min_value, "X"].iloc[0]],
-                   popup=f"Menor valor: {min_value}<br>{min_municipio}",
-                   icon=folium.Icon(color="purple", icon="arrow-down"),
-                   ).add_to(m)
+folium.Marker([data.loc[data[ind] == max_value, "Y"].iloc[0],
+               data.loc[data[ind] == max_value, "X"].iloc[0]],
+               popup=f"Maior valor: {max_value}<br>{max_municipio}",
+               icon=folium.Icon(color="darkpurple", icon="arrow-up"),
+              ).add_to(m)
+folium.Marker([data.loc[data[ind] == min_value, "Y"].iloc[0],
+              data.loc[data[ind] == min_value, "X"].iloc[0]],
+              popup=f"Menor valor: {min_value}<br>{min_municipio}",
+              icon=folium.Icon(color="purple", icon="arrow-down"),
+              ).add_to(m)
     
-    m.to_streamlit
+m.to_streamlit
