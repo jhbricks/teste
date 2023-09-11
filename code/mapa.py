fe3515509aa19e3d4d015254221f5773a576325e
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 12:33:12 -0300

    Update mapa.py

--- code/mapa.py
+++ code/mapa.py
@@ -11,8 +11,17 @@ import matplotlib.pyplot as plt
 
 import streamlit as st
 
-if st.button('Button 1'):
-    st.write('Button 1 was clicked')
-    if st.button('Button 2'):
-        # This will never be executed.
-        st.write('Button 2 was clicked')
+import streamlit as st
+st.markdown(""" div.stButton > button:first-child {
+background-color: #00cc00;color:white;font-size:20px;height:3em;width:50%;border-radius:10px 10px 10px 10px;
+}
+“”", unsafe_allow_html=True)
+
+col1,col2,col3=st.beta_columns([0.3,1.2,0.3])
+with col1:
+    st.empty()
+with col2:
+    if st.button(“the notice you want to show”):
+        st.write(“content you want to show”)
+with col3:
+    st.empty()
